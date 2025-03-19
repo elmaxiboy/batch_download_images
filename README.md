330d0f931c748ad8063e26033b1ff4a96022b144
elmaxiboy <maxescovi@gmail.com>
2025-03-19 12:16:48 +0100

    chore: directory navigation

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # Pexels images scraper
 
-This tool is intended to batch download images from Pexels website in high resolution, to be used in training Computer Vision models.
+This tool is intended to batch download images from Pexels website in high resolution. It is part of a personal project to learn and implement Computer Vision models.
+
+It's designed to get the maximum resolution currently available.
 
 ## Activate virtual environment and load packages:
 
 ``` sh
 source .venv/bin/activate
 ```
+## Or Install requirements
+``` sh
+pip install -r requirements.txt
+```
 
 ## Scrape website and get image URLs: 
 
 ``` sh
-python pexels_scraper.py {YOUR_URL}
+python pexels_scraper.py --keyword {YOUR_KEYWORD} --output-folder {NAME_OF_OUTPUT_FOLDER}
 ```
+
--- pexels_scraper.py
+++ pexels_scraper.py
@@ -3,12 +3,37 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
-import sys
+import logging
+import argparse
+from datetime import datetime
+import requests
+
+def create_output_folder(path: str)-> str:
+    """Ensures the specified directory exists, creating it if necessary."""
+    try:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+    except Exception as e:
+        logging.error(f"Error creating folder {path}: {e}")        
+        
+    return path
+
+def remove_file(path: str)-> str:
+    try:
+        os.remove(path)
+        logging.debug("Output file removed")
+    except:
+        logging.debug("Output file was already removed")
+
+def download_image (path, image_url, name):
+    img_data = requests.get(image_url).content
+    with open(path+'/'+str(name)+'.jpeg', 'wb') as handler:
+        handler.write(img_data)
 
 def get_new_height(driver):
+
     # Scroll down using PAGE_DOWN multiple times
     for _ in range(50):  # Simulate a user scrolling gradually
         driver.find_element(By.TAG_NAME, "body").send_keys(Keys.PAGE_DOWN)
@@ -16,57 +41,94 @@ def get_new_height(driver):
 
     new_height = driver.execute_script("return document.body.scrollHeight")
 
+    logging.debug("New height is: {}",new_height)
+
     return new_height
 
-def scrape_pexels_images(search_url, max_scrolls=100):
+def scrape_pexels_images(keyword,output_folder,max_scrolls=100,):
+
     # Setup Selenium WebDriver
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")  # Run in headless mode
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
-    driver.get(search_url)
-    
-    image_urls = set()
-    last_height = driver.execute_script("return document.body.scrollHeight")
+    driver.get("https://www.pexels.com/search/"+keyword)
 
     try:
-        os.remove("output.txt")
-        print("output file removed")
-    except:
-        print("output file was already removed")
+        while True:
 
-    with open('output.txt', 'w') as f:
-        for _ in range(max_scrolls):
-            # Extract image elements
-            images = driver.find_elements(By.CSS_SELECTOR, "img[srcset]")
+            logging.info("Scraping process is running... press CTRL-C to quit.")
 
-            for img in images:
-                img_url = img.get_attribute("srcset")
-                    
-                start = img_url.find('1200w, ') + 7
-                end = img_url.find(' 1600w', start)
-                url_max_res= img_url[start:end]
-                if url_max_res not in image_urls:
-                    image_urls.add(url_max_res)
-                    print(url_max_res)
-                    f.write(url_max_res+"\n")
+            image_urls = set()
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            counter=0
 
-            new_height= get_new_height(driver)
+            create_output_folder(output_folder)
 
-            if  new_height== last_height:
-                print("Reached end of page or no new images found.")
-                break  # Stop scrolling if no more content is loading
-            last_height = new_height  # Update last height
+            kw_output_folder=create_output_folder(output_folder+"/"+keyword)
 
+            remove_file(kw_output_folder+"/output.txt")
+
+            with open(kw_output_folder+"/output.txt", 'w') as f:
+                for _ in range(max_scrolls):
+                    # Extract image elements
+                    images = driver.find_elements(By.CSS_SELECTOR, "img[srcset]")
+                    new_images = len(images)-counter
+                    logging.info("Found " +str(new_images)+' new images')
+                    for img in images:
+
+                        img_url = img.get_attribute("srcset")        
+                        start = img_url.find('1200w, ') + 7
+                        end = img_url.find('?auto=compress&cs=tinysrgb&w=1600', start)
+                        url_max_res= img_url[start:end]
+
+                        if url_max_res not in image_urls:
+
+                            image_urls.add(url_max_res)
+
+                            logging.debug("Downloading: " +url_max_res)
+
+                            download_image(kw_output_folder,url_max_res,counter)
+                            f.write(url_max_res+"\n")
+                            counter+=1
+
+
+                    new_height= get_new_height(driver)
+
+                    if  new_height== last_height:
+                        logging.info("Reached end of page or no new images found.")
+                        break  # Stop scrolling if no more content is loading
+                    last_height = new_height  # Update last height
+
+                
+                return list(image_urls)    
+
+    except KeyboardInterrupt:
+        logging.info("Interrupted by user...")
+    finally:
+        logging.info("Closing Selenium WebDriver")
         driver.quit()
-        return list(image_urls)
+        logging.info("Tschüss!")        
+        
+
+   
 
 if __name__ == "__main__":
     
-    url = sys.argv[1]
-    
-    print ("Scraping Pexel website for images URL {}".format(url))
+    logging.basicConfig(level=logging.INFO)
 
-    image_links = scrape_pexels_images(url)
-
+    # Initialize the parser
+    parser = argparse.ArgumentParser(description='Scraper for licence-free images')
     
+    # Add arguments
+    parser.add_argument('--keyword', action="store", dest='keyword', default='doves')
+    parser.add_argument('--output-folder', action="store", dest='output_folder', default='output')
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    logging.info("Scraping Pexel website for images of {}".format(args.keyword))
+
+    scrape_pexels_images(args.keyword, args.output_folder)
+
+
     
